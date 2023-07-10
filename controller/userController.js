33d3c026e2a73c0644082a48e022d48de60c68c2
Bopanna99 <bopannachettimada@gmail.com>
2023-07-10 13:45:01 +0530

    first commit

---
+++ controller/userController.js
@@ -0,0 +1,17 @@
+const userController = {
+
+    index : (req,res)=>{
+        res.render('index.ejs')
+    },
+    new:(req,res)=>{
+        res.render('create.ejs')
+    },
+    edit :(req,res)=>{
+        res.render('edit.ejs')
+    },
+    pnf :(req,res)=>{
+        res.render('pnf.ejs')
+    }
+}
+
+module.exports = userController
